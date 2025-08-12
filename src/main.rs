b6d0c6433a2ebe5f33d98137b1226ff61657c934
Marshall Ku / 구영표 <marshall@kakao.com>
2025-08-13 00:17:58 +0900

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,64 @@
+use axum::serve;
+use env::state::AppState;
+use routes::app::app;
+use tokio::{net::TcpListener, signal};
+use tower_http::trace::{DefaultMakeSpan, DefaultOnResponse, TraceLayer};
+use tracing::{info, Level};
+use utils::log::trace_layer_on_request;
+
+mod env;
+mod routes;
+mod utils;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt()
+        .with_target(false)
+        .compact()
+        .init();
+
+    let state = AppState::from_env();
+    let address = format!("{}:{}", state.host, state.port);
+    let app = app()
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
+                .on_response(DefaultOnResponse::new().level(Level::INFO))
+                .on_request(trace_layer_on_request),
+        )
+        .with_state(state);
+    let listener = TcpListener::bind(address.as_str()).await.unwrap();
+
+    info!("Listening on http://{}", address);
+
+    serve(listener, app.into_make_service())
+        .with_graceful_shutdown(handle_shutdown())
+        .await
+        .unwrap();
+}
+
+async fn handle_shutdown() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    info!("Shutting down...");
+}
